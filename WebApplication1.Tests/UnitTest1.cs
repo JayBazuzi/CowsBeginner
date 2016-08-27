7b25357520a739ebdba975675480945d9808c0d2
Jay Bazuzi <JAY@BAZUZI.COM>
2016-08-26 22:49:55 -0700

    Sanity test

--- WebApplication1.Tests/UnitTest1.cs
+++ WebApplication1.Tests/UnitTest1.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WebApplication1.Tests
@@ -7,8 +12,17 @@ namespace WebApplication1.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task SanityTest()
         {
+            var configuration = new HttpConfiguration();
+            WebApiConfig.Register(configuration);
+            var httpClient = new HttpClient(new HttpServer(configuration));
+
+            IEnumerable<string> results;
+            (await httpClient.GetAsync("http://example.com/api/values")).EnsureSuccessStatusCode()
+                .TryGetContentValue(out results)
+                .Should().BeTrue();
+            results.Should().BeEquivalentTo("value1", "value2");
         }
     }
 }
