b5972046fdb410c5e96df4e6594430cd82205a9a
Jay Bazuzi <JAY@BAZUZI.COM>
2016-08-27 22:37:44 -0700

    Remove System.IdentityModel.Tokens.Jwt

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
-using System.Web.Routing;
-using Microsoft.Practices.Unity;
 
 namespace WebApplication1
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
